d4862c0db83379555ea241cca478e20a75759977
Cesar Funes <fuunesto@gmail.com>
2022-09-19 18:12:20 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export type Post = {
+  description: string
+  name: string
+  image: {
+    _type: string
+    asset: {}
+  }
+  id: string
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const url =
+    "https://cteelkpu.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'rotmgPost'%5D%20%7B%0A%22id%22%3A%20_id%2C%0A%20%20%22name%22%3A%20name%2C%0A%20%20%22description%22%3A%20description%2C%0A%20%20%20%22image%22%3A%20image%0A%7D"
+  const response = await fetch(url)
+  const posts: Post[] = (await response.json()).result
+
+  return {
+    props: { posts },
+  }
+}
+
+const Home: NextPage<{ posts: Post[] }> = ({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <div className={styles.container}>
+      <h1>Artists:</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.name}>
+            <Link href={`/post/${post.id}`}>
+              <a>{post.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,45 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import Link from 'next/link'
+import { Post } from '..'
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const url =
+    "https://cteelkpu.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'rotmgPost'%5D%20%7B%0A%22id%22%3A%20_id%2C%0A%20%20%22name%22%3A%20name%2C%0A%20%20%22description%22%3A%20description%2C%0A%20%20%20%22image%22%3A%20image%0A%7D"
+  const response = await fetch(url)
+  const posts: Post[] = (await response.json()).result
+
+  return {
+    paths: posts.map((post) => ({
+      params: { id: post.id },
+    })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<{ post: Post }> = async ({
+  params,
+}) => {
+  const path = params?.id
+
+  const url = `https://cteelkpu.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'rotmgPost'%20%26%26%20_id%3D%3D'${path}'%5D%20%7B%0A%20%20%22id%22%3A%20_id%2C%0A%20%20%22name%22%3A%20name%2C%0A%20%20%22image%22%3A%20image%2C%0A%20%20%22description%22%3A%20description%0A%7D`
+
+  const response = await fetch(url)
+  const post: Post = (await response.json()).result[0]
+
+  return {
+    props: { post },
+  }
+}
+
+const id = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(post)
+
+  return (
+    <div>
+      <Link href='/'>Go back</Link>
+      <h1>Name: {post?.name}</h1>
+      <p>Desc: {post?.description}</p>
+    </div>
+  )
+}
+export default id
