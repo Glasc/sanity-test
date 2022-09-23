a2d9525565f973f2d9c62f6ef792ec4d384fe37b
Cesar Funes <fuunesto@gmail.com>
2022-09-23 13:33:01 -0500

    :

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,13 +15,12 @@ export type Post = {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://sanity-test-sage.vercel.app/api/revalidate')
+  
   const url =
     "https://cteelkpu.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'rotmgPost'%5D%20%7B%0A%22id%22%3A%20_id%2C%0A%20%20%22name%22%3A%20name%2C%0A%20%20%22description%22%3A%20description%2C%0A%20%20%20%22image%22%3A%20image%0A%7D"
   const response = await fetch(url)
   const posts: Post[] = (await response.json()).result
-
-  const res = await fetch('https://sanity-test-sage.vercel.app/api/revalidate')
-  console.log(res)
 
   return {
     props: { posts },
