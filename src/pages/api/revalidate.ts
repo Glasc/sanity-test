bf8622a1cbcca3eccc3d5c32fca379967775144a
Cesar Funes <fuunesto@gmail.com>
2022-09-23 13:41:56 -0500

    :

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,14 +6,6 @@ export default async function handler(
 ) {
   // Check for secret to confirm this is a valid request
 
-  try {
-    // this should be the actual path not a rewritten path
-    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate('/')
-    return res.json({ revalidated: true })
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
-  }
+  await res.revalidate('/')
+  return res.json({ revalidated: true })
 }
