834431c78dab9c772244442775e80091c4411368
Funes Avila Cesar de Jesus <fuunesto@gmail.com>
2022-09-23 08:54:25 -0500

    .

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -11,7 +11,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: posts?.map((post) => ({
       params: { id: post.id },
     })),
-    fallback: false,
+    fallback: true,
   }
 }
 
